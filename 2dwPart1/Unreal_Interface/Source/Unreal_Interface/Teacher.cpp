4e4a37fe8ef88704a6786ce7c5bc6f37fb1fbbde
WEBAN <zkzk1231@gmail.com>
2024-09-27 04:55:20 +0900

    real files

---
+++ 2dwPart1/Unreal_Interface/Source/Unreal_Interface/Teacher.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Teacher.h"
+
+UTeacher::UTeacher()
+{
+	Name = TEXT("Teacher");
+}
+
+void UTeacher::DoLesson()
+{
+	ILessonInterface::DoLesson();
+	UE_LOG(LogTemp,Log,TEXT("%s 님은 가르칩니다."), *Name);
+}
